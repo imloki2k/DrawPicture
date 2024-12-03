a70b176568f200e686c17ee40986f365446028ec
tmquang <tmquang1@cmc.com.vn>
2024-12-04 00:28:17 +0700

    ip

---
+++ Controllers/PictureCollectionController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrawPicture.Controllers
+{
+    public class PictureCollectionController : Controller
+    {
+        public IActionResult ShowMyPicture()
+        {
+            var imageFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "topic");
+
+            if (!Directory.Exists(imageFolderPath))
+            {
+                return View(new List<string>()); 
+            }
+
+            var directories = Directory.GetDirectories(imageFolderPath)
+                                       .Select(folder => $"{Path.GetFileName(folder)}") 
+                                       .ToList();
+
+            return View(directories);
+        }
+
+        public IActionResult DisplayCollection()
+        {
+            var imageFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "topic");
+
+            if (!Directory.Exists(imageFolderPath))
+            {
+                return View(new List<string>()); 
+            }
+
+            var directories = Directory.GetDirectories(imageFolderPath)
+                                       .Select(folder => $"{Path.GetFileName(folder)}") 
+                                       .ToList();
+
+            return View(directories);
+        }
+    }
+}
